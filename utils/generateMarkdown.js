2f916091c0ecdcb68d695064a66159269b08e970
Cassie Sprague <cassie.sprague.18@gmail.com>
2024-02-28 15:47:27 -0500

    started writing functions in generate markdown and added comment to how i got the functions. have not tested, will test when done to find any errors. still working on

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,29 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Used the following link to help with these functions. Have not tested them out yet but will test when done and will change if they do not work.
+// https://www.lambdatest.com/automation-testing-advisor/javascript/redwood-licenseInfo
+function renderLicenseBadge(license) {
+  return selectLicense(license).badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseText;
+  if (license != "No License"){
+    licenseText = "This project is licensed under the ${selectLicense(license).fullName}; A copy of the license can be found at: ${selectLicense(license).link}"
+  }
+  else {
+    licenseText = "Not Licensed";
+  }
+  return licenseText;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
