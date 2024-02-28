3b9d446a8252c53f3567b9cce7320eeb2a9a2735
Cassie Sprague <cassie.sprague.18@gmail.com>
2024-02-28 09:45:21 -0500

    added all const needed to continue working, added comments for the information added and links to sites I used to help me find the information for correct formating for the const

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 // TODO: Include packages needed for this application
+// Below is the const necessary for the application which are the Inquire Package, fs which 
+// is for Node.js, utils folder which holds the generateMarkdown, and generateMarkdown which
+// allows marked text to be converted to various formats. Along with going through the class activities,
+// I also used the following links to help me find the correct format for the below const.
+// https://www.w3schools.com/nodejs/nodejs_modules.asp
+// https://www.w3schools.com/nodejs/nodejs_url.asp
+
+const inquirer = requirer("inquirer");
+const fs = require("fs");
+const utils = reauire("util");
+const genMkdn = require(".utils/generateMarkdown");
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
