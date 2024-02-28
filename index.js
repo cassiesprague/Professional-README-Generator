e127b3c49c63a7823407164c07cbc562b614db58
Cassie Sprague <cassie.sprague.18@gmail.com>
2024-02-28 13:52:19 -0500

    added , after the end of every question, ran in to that error when trying to run it

--- index.js
+++ index.js
@@ -24,63 +24,63 @@ const questions = [
         type: "input",
         message: "What is the title of your project?",
         name: "title",
-    }
+    },
     {
         type: "input",
         message: "What is a short description of your project? Explain what the motivation was of your project and why you built it.",
         name: "description",
-    }
+    },
     {
         type: "input",
         message: "",
         name: "",
-    }
+    },
     {
         type: "input",
         message: "What are the steps required to install your project?",
         name: "installation",
-    }
+    },
     {
         type: "input",
         message: "What are the instructions and examples for usage?",
         name: "usage",
-    }
+    },
     {
         type: "input",
         message: "If there were any collaborators for this project, list them here. Also list any third-party assets or links you used here.",
         name: "credits",
-    }
+    },
     {
         type: "input",
         message: "What license did your project have?",
         name: "license",
         choices: ["Apache", "ISC", "MIT", "No License"]
-    }
+    },
     {
         type: "input",
         message: "Does your project have any badges? If so, list them here.",
         name: "badges",
-    }
+    },
     {
         type: "input",
         message: "List the features of your project here.",
         name: "features",
-    }
+    },
     {
         type: "input",
         message: "Would you like your project to allow others to contribute to it? If so, describe here how to contribute.",
         name: "contributing",
-    }
+    },
     {
         type: "input",
         message: "Did you write tests for your project? If so, describe here how to run them.",
         name: "tests",
-    }
+    },
     {
         type: "input",
         message: "What is your GitHub username?",
         name: "username",
-    }
+    },
     {
         type: "input",
         message: "What is your email address?",
