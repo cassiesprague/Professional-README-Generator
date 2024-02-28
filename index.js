67090d4903d50e3f5c6a97f331146c6e818cacf4
Cassie Sprague <cassie.sprague.18@gmail.com>
2024-02-28 12:23:06 -0500

    used the link provided in the comment to figure out the questions to ask for a professional readme. need to ask a few more questions required. still working on

--- index.js
+++ index.js
@@ -14,12 +14,21 @@ const genMkdn = require(".utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 // Below is the questions array that allows users to input their answers. I looked back at the class
-// activities and found how to do the array in 19-Ins_Inquirer-Demo.
+// activities and found how to do the array in 19-Ins_Inquirer-Demo. I used the given link below to look
+// at what a professional readme should contain and added all fields even if some are not applicable at
+// the moment.
+// https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide
+
 const questions = [
     {
         type: "input",
-        message: "",
-        name: "",
+        message: "What is the title of your project?",
+        name: "title",
+    }
+    {
+        type: "input",
+        message: "What is a short description of your project? Explain what the motivation was of your project and why you built it.",
+        name: "description",
     }
     {
         type: "input",
@@ -28,38 +37,43 @@ const questions = [
     }
     {
         type: "input",
-        message: "",
-        name: "",
+        message: "What are the steps required to install your project?",
+        name: "installation",
     }
     {
         type: "input",
-        message: "",
-        name: "",
+        message: "What are the instructions and examples for usage?",
+        name: "usage",
     }
     {
         type: "input",
-        message: "",
-        name: "",
+        message: "If there were any collaborators for this project, list them here. Also list any third-party assets or links you used here.",
+        name: "credits",
     }
     {
         type: "input",
-        message: "",
-        name: "",
+        message: "What license did your project have?",
+        name: "license",
     }
     {
         type: "input",
-        message: "",
-        name: "",
+        message: "Does your project have any badges? If so, list them here.",
+        name: "badges",
     }
     {
         type: "input",
-        message: "",
-        name: "",
+        message: "List the features of your project here.",
+        name: "features",
     }
     {
         type: "input",
-        message: "",
-        name: "",
+        message: "Would you like your project to allow others to contribute to it? If so, describe here how to contribute.",
+        name: "contributing",
+    }
+    {
+        type: "input",
+        message: "Did you write tests for your project? If so, describe here how to run them.",
+        name: "tests",
     }
 ];
 
