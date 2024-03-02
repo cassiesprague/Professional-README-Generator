2d34a47bb520782acb16da32fc21e354add8551a
Cassie Sprague <cassie.sprague.18@gmail.com>
2024-03-02 17:36:13 -0500

    added comments and links for new render license functions. links for badges and copywrites for licenses for function and empty srting for no license

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,8 +2,23 @@
 // If there is no license, return an empty string
 // Used the following link to help with these functions. Have not tested them out yet but will test when done and will change if they do not work.
 // https://www.lambdatest.com/automation-testing-advisor/javascript/redwood-licenseInfo
+// Used the following website to get a template for making badges and made my own based on the templates for the badge options I choose
+// https://shields.io/badges
+// Used the following website to find the specific licenses I have options for
+// https://opensource.org
 function renderLicenseBadge(license) {
-  return selectLicense(license).badge;
+  if (license === "Apache") {
+    return '![License: Apache](https://img.shields.io/badge/License-Apache-blue)(https://opensource.org/license/apache)'
+  }
+  if (license === "ISC") {
+    return '![License: Apache](https://img.shields.io/badge/License-ICS-yellow)(https://opensource.org/license/isc)'
+  }
+  if (license === "MIT") {
+    return '![License: Apache](https://img.shields.io/badge/License-MIT-green)(https://opensource.org/license/mit)'
+  }
+  if (license === "No Lincese") {
+    return ''
+  }
 }
 
 // TODO: Create a function that returns the license link
